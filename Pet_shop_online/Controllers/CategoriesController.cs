a2a1018e8e42ffea7b401ad2c7d4e55fba010a67
Denisa Iordache <61270889+denisaiordache@users.noreply.github.com>
2020-11-11 19:56:59 +0200

    modificare Categories

--- Pet_shop_online/Controllers/CategoriesController.cs
+++ Pet_shop_online/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pet_shop_online.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,96 @@ namespace Pet_shop_online.Controllers
 {
     public class CategoriesController : Controller
     {
+        private Models.AppContext db = new Models.AppContext();
+
         // GET: Categories
         public ActionResult Index()
         {
+            var categories = db.Categories;
+            ViewBag.Categories = categories;
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+            }
+
             return View();
+        }
+
+
+        public ActionResult Show(int id)
+        {
+            Category category = db.Categories.Find(id);
+            ViewBag.Category = category;
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public ActionResult New(Category category)
+        {
+            try
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                TempData["message"] = "Categoria a fost adaugata cu succes!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View(category);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return View(category);
+
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Category requestCategory)
+        {
+
+            try
+            {
+                Category category = db.Categories.Find(id);
+                if (TryUpdateModel(category))
+                {
+                    category = requestCategory;
+                    db.SaveChanges();
+                    TempData["message"] = "Categoria a fost modificata cu succes!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(requestCategory);
+            }
+
+            catch (Exception e)
+            {
+                return View();
+            }
+
+
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            TempData["message"] = "Categoria a fost eliminata!";
+            return RedirectToAction("Index");
         }
     }
 }
